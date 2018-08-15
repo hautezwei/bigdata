73ed6db451eeb6664b348a9a8ed8f6e7d40c0a9b
Zhengchuanjun <zhengchuanjun@putao.com>
2018-08-15 14:49:12 +0800

    init msg

---
+++ pandas.py
@@ -0,0 +1,21 @@
+import pandas
+import numpy as np
+import py_compile
+
+py_compile.compile(r'pandas.py')
+
+print("activity")
+
+arr1 = np.arange(10)
+
+print(arr1)
+
+print(type(arr1))
+
+arr2 = np.array(np.arange(12)).reshape(4, 3)
+
+print(arr2)
+
+print(type(arr2))
+
+dic2 = {'a': [1, 2, 3, 4], 'b': [5, 6, 7, 8], 'c': [9, 10, 11, 12], 'd': [13, 14, 15, 16]}
