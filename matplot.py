fd4ca7e38f7b5a1a1074c9cfaf24d1319dd8d51c
DenisZen <charlesdavidwince@gmail.com>
2018-08-15 15:33:37 +0800

    add file

---
+++ matplot.py
@@ -0,0 +1,10 @@
+import matplotlib
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+matplotlib.use('GTKAgg')
+plt.plot(np.arange(100))
+
+plt.show()
--- pandas.py
+++ pandas.py
@@ -22,4 +22,10 @@ print(arr2)
 
 print(type(arr2))
 
+print(type(arr2))
+
 dic2 = {'a': [1, 2, 3, 4], 'b': [5, 6, 7, 8], 'c': [9, 10, 11, 12], 'd': [13, 14, 15, 16]}
+
+"""this is a godd 
+
+"""
